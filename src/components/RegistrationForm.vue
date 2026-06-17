593768fcf441c77ae5d3d4a1e24bee89c2a20d13
VickyMastro <vicmastroianni1702@gmail.com>
2026-06-17 18:37:21 -0300

    Agrego valibot para los inputs de las contraseñas

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,4 +1,6 @@
 <script setup>
+import * as v from 'valibot'
+import { emailField, passwordField, usernameField } from '../utils/schemas.js'
 import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
 import { useUserStore } from '../stores/userStore.js'
@@ -11,10 +13,21 @@ import AuthLink from './base/links/AuthLink.vue'
 const userStore = useUserStore()
 const router = useRouter()
 
+const schema = v.object({
+  name: usernameField,
+  email: emailField,
+  password: passwordField,
+  repeatPassword: v.pipe(
+    v.string(),
+    v.custom((val) => val === registerData.password, 'Las contraseñas no coinciden'),
+  ),
+})
+
 const registerData = reactive({
   name: '',
   email: '',
   password: '',
+  repeatPassword: '',
 })
 
 async function registerButton() {
@@ -29,22 +42,27 @@ async function registerButton() {
 
 <template>
   <div
-    class="relative z-10 w-full max-w-sm bg-zinc-900 border border-zinc-700 rounded-2xl p-8 flex flex-col gap-4"
+    class="relative z-10 w-[384px] h-auto bg-zinc-900 border border-zinc-700 rounded-2xl p-6 flex flex-col gap-2"
   >
     <h1 class="font-bold text-2xl text-center">Crear cuenta</h1>
     <p class="text-center text-neutral-400 text-sm">
       Registrate para guardar tu contenido favorito
     </p>
+    <UForm class="flex flex-col" :state="registerData" :schema="schema">
+      <NameInput v-model="registerData.name" />
+      <EmailInput v-model="registerData.email" />
+      <PasswordInput v-model="registerData.password" />
+      <PasswordInput
+        v-model="registerData.repeatPassword"
+        name="repeatPassword"
+        label="Confirmar contraseña"
+      />
 
-    <NameInput v-model="registerData.name" />
-    <EmailInput v-model="registerData.email" />
-    <PasswordInput v-model="registerData.password" />
-    <PasswordInput label="Confirmar contraseña" />
-
-    <AuthButton label="Crear cuenta" @click="registerButton" />
-    <div class="flex justify-center text-sm">
-      <span class="mr-1">¿Ya tienes cuenta?</span>
-      <AuthLink title="Inicia sesión" to="/auth" />
-    </div>
+      <AuthButton label="Crear cuenta" @click="registerButton" />
+      <div class="flex justify-center text-sm pt-3">
+        <span class="mr-1">¿Ya tienes cuenta?</span>
+        <AuthLink title="Inicia sesión" to="/auth" />
+      </div>
+    </UForm>
   </div>
 </template>
